01504d268e4af67658e666fd7e0a880ee58b1c48
Arturs <xyzdelete@protonmail.com>
2024-02-17 11:46:33 +0200

    15. Practice- Guess My Number

---
+++ 2.GettingStarted/2-7GuessNumber/2-7GuessNumber/main.cpp
@@ -0,0 +1,39 @@
+#include <cstdlib> // srand, rand
+#include <iostream>
+
+int main()
+{
+    std::srand(std::time(nullptr));
+
+    int random_number{ std::rand() };
+
+    std::cout << "number: " << random_number << std::endl;
+    std::cout << "RAND_MAX: " << RAND_MAX << std::endl;
+
+    int secret_number = random_number % 10 + 1;
+
+    //std::cout << "secret_number: " << secret_number << std::endl;
+
+    int guess_number{};
+
+    //Ask the user to guess
+    std::cout << "Guess my number ( 1-10 ) :";
+    do
+    {
+        std::cin >> guess_number;
+        if (secret_number < guess_number)
+        {
+            std::cout << "The number is lower than that" << std::endl;
+        }
+
+        if (secret_number > guess_number)
+        {
+            std::cout << "The number is higher than that" << std::endl;
+        }
+    }
+    while (guess_number != secret_number);
+
+    std::cout << "Congratulations, the  number is :" << guess_number << std::endl;
+
+    return 0;
+}
