a92449e43e6b255e879e493fb245a87079d3f46e
Arturs <xyzdelete@protonmail.com>
2024-02-29 20:11:43 +0200

    92. QNetworkAccessManager

---
+++ 12.QtNetworkOfferings/12-2QNetworkAccessManagerDemo/12-2QNAMD/widget.cpp
@@ -0,0 +1,46 @@
+#include "widget.hpp"
+#include "./ui_widget.h"
+#include <QNetworkReply>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    net_manager = new QNetworkAccessManager{ this };
+    net_reply = nullptr;
+    m_data_buffer = new QByteArray{};
+
+    QNetworkRequest request{};
+    request.setUrl(QUrl{ "https://www.github.com" });
+
+    net_reply = net_manager->get(request);
+
+    connect(net_reply, &QIODevice::readyRead, this, &Widget::dataReadyToRead);
+    connect(net_reply, &QNetworkReply::finished, this, &Widget::dataReadFinished);
+}
+
+Widget::~Widget()
+{
+    delete m_data_buffer;
+    delete ui;
+}
+
+void Widget::dataReadyToRead()
+{
+    qDebug() << "Some data available";
+    m_data_buffer->append(net_reply->readAll());
+}
+
+void Widget::dataReadFinished()
+{
+    if (net_reply->error())
+    {
+        qDebug() << "Error: " << QString{ *m_data_buffer };
+    }
+    else
+    {
+        ui->textEdit->setPlainText(QString{ *m_data_buffer });
+    }
+}
